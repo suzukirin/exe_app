894de1004848146e09cf797131be74034608846c
suzukirin <qilin.liberty124@gmail.com>
2021-07-03 21:56:37 +0900

    first commit

---
+++ 06_php/server/01/hello_br.php
@@ -0,0 +1,11 @@
+<?php
+
+echo 'Hello World';
+
+echo '<br>';
+
+echo '今日の天気は晴れ';
+
+echo '<br>';
+
+echo '私の名前はKenです';
---
+++ 06_php/server/01/variable_1.php
@@ -0,0 +1,7 @@
+<?php
+
+$message = 'Hello World'; // 'Hello World' という値を $message に格納している
+echo $message . '<br>';
+
+$name = 'Ken'; // 'Ken' という値を $name に格納している
+echo '私の名前は' . $name . 'です';
